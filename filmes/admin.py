88707d2b63906c191691a958f45863af1660d45c
spelicano <spelicano@bol.com.br>
2025-06-03 14:49:25 -0300

    ManyToMany

--- filmes/admin.py
+++ filmes/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from filmes.models import Filme
 from filmes.models import Genero
+from filmes.models import Ator
 
 class FilmeAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'ano', 'genero', 'diretor', 'atores')
-    search_fields = ('titulo', 'ano', 'genero', 'diretor', 'atores')
+    list_display = ('titulo', 'ano', 'genero', 'diretor')
+    search_fields = ('titulo', 'ano', 'diretor')
+    filter_horizontal = ('atores',)
+admin.site.register(Filme, FilmeAdmin)   
 
-admin.site.register(Filme, FilmeAdmin)    
+class AtorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_nascimento')
+    search_fields = ('nome',)
+admin.site.register(Ator, AtorAdmin)
 
 class GeneroAdmin(admin.ModelAdmin):
     list_display = ('nome',)
     search_fields = ('nome',)
-
 admin.site.register(Genero, GeneroAdmin)
